deeeb64e5ffde06811381175217f50ffcdcdbe6f
Shivank Kapur <shivankkapur2004@gmail.com>
2024-02-23 02:29:17 +0530

    backend done

--- server/src/managers/RoomManager.ts
+++ server/src/managers/RoomManager.ts
@@ -2,6 +2,8 @@ import { User } from "./userManager";
 
 let GLOBAL_ROOM_ID = 1;
 
+// In RoomManager, we manage people in a room. As a room is created, a person sends a send-offer packet. Then this person sends his WebRTC Configuration.
+// When the server receives these responses, which is initHandlers method then it tells the user2 the user1 has sent an offer, now you can accept/reject it. Now, when user2 receives an offer they have to send back an anwer to user1.
 export interface Room {
   user1: User;
   user2: User;
@@ -15,12 +17,28 @@ export class RoomManager {
   }
 
   createRoom(user1: User, user2: User) {
-    const roomId = this.generate();
+    const roomId = this.generate().toString();
     this.rooms.set(roomId.toString(), {
       user1,
       user2,
     });
-    user1.socket.emit("send-offer");
+    user1.socket.emit("new-room", {
+      roomId,
+    });
+  }
+
+  onOffer(roomId: string, sdp: string) {
+    const user2 = this.rooms.get(roomId)?.user2;
+    user2?.socket.emit("offer", {
+      sdp,
+    });
+  }
+
+  onAnswer(roomId: string, sdp: string) {
+    const user1 = this.rooms.get(roomId)?.user1;
+    user1?.socket.emit("offer", {
+      sdp,
+    });
   }
 
   generate() {
--- server/src/managers/userManager.ts
+++ server/src/managers/userManager.ts
@@ -1,16 +1,20 @@
 import { Socket } from "socket.io";
+import { RoomManager } from "./RoomManager";
 
 export interface User {
   socket: Socket;
   name: String;
 }
 
+// Multiple people make web socket connection, and each of them are stored in a UserManager Class and as people join they're stored in a queue to match with another person. Time to time we take out two people from this queue and pull them to RoomManager. There folks are matched.
 export class UserManager {
   private users: User[];
   private queue: string[];
+  private roomManager: RoomManager;
   constructor() {
     this.users = [];
     this.queue = [];
+    this.roomManager = new RoomManager();
   }
   addUser(name: string, socket: Socket) {
     this.users.push({
@@ -19,9 +23,11 @@ export class UserManager {
     });
     this.queue.push(socket.id);
     this.clearQueue();
+    this.initHandlers(socket);
   }
-  removeUser(socketId) {
-    this.users = this.users.filter((x) => x.socket.id === socketId);
+  removeUser(socketId: string) {
+    const user = this.users.find(x => x.socket.id === socketId)
+    this.users = this.users.filter((x) => x.socket.id !== socketId);
     this.queue = this.queue.filter((x) => x === socketId);
   }
   clearQueue() {
@@ -30,10 +36,19 @@ export class UserManager {
     }
     const user1 = this.users.find((x) => x.socket.id === this.queue.pop());
     const user2 = this.users.find((x) => x.socket.id === this.queue.pop());
-    const roomId = this.generate();
-    user1?.socket.emit("new-room", {
-      type: "send-offer",
-      room: roomId,
-    });
+
+    if (!user1 || !user2) {
+      return;
+    }
+
+    const room = this.roomManager.createRoom(user1, user2);
+  }
+  initHandlers(socket: Socket) {
+    socket.on("offer", { sdp, roomId }: { sdp: string, roomId: string }) => {
+      this.roomManager.onOffer(roomId, sdp, socket.id);
+    }
+    socket.on("answer", { sdp, roomId }: { sdp: string, roomId: string }) => {
+      this.roomManager.onAnswer(roomId, sdp, socket.id);
+    }
   }
 }
